c7a75f9c9dfd27fd22e7d9931365c06962b6167d
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2024-12-14 19:04:30 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,94 +1,124 @@
-import logging
-import logging.config
-import warnings
-from pyrogram import Client, idle
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from config import Config
-from aiohttp import web
-from pytz import timezone
-from datetime import datetime
-import asyncio
-from plugins.web_support import web_server
-from plugins.file_rename import app
-import pyromod
-
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from pyrogram import Client, filters
+from pyrogram.types import *
+from motor.motor_asyncio import AsyncIOMotorClient  
+from os import environ as env
+import asyncio, datetime, time
 
 
-class Bot(Client):
+ACCEPTED_TEXT = "{user},\n\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝗌𝗍 𝖳𝗈 𝖩𝗈𝗂𝗇 {chat} 𝖺𝗌 𝖻𝖾𝖾𝗇 𝖠𝖼𝖼𝖾𝗉𝗍𝖾𝖽."
+START_TEXT = "{},\n\n𝖨 𝖼𝖺𝗇 𝖺𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝖼𝖺𝗅𝗅𝗒 𝖺𝗉𝗉𝗋𝗈𝗏𝖾 𝗎𝗌𝖾𝗋𝗌 𝗂𝗇 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌.\n\n𝖩𝗎𝗌𝗍 𝖺𝖽𝖽 𝗆𝖾 𝗂𝗇 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌 𝗐𝗂𝗍𝗁 𝗉𝖾𝗋𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝗍𝗈 𝖺𝖽𝖽 𝗇𝖾𝗐 𝗆𝖾𝗆𝖻𝖾𝗋𝗌.\n\n**<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Mikoyo'>Mikoyo</a></blockquote>**"
 
-    def __init__(self):
-        super().__init__(
-            name="SnowRenamer",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=200,
-            plugins={"root": "plugins"},
-            sleep_threshold=15,
+API_ID = int(env.get('API_ID', ''))
+API_HASH = env.get('API_HASH', '')
+BOT_TOKEN = env.get('BOT_TOKEN', '')
+DB_URL = env.get('DB_URL', '')
+ADMINS = int(env.get('ADMINS', ''))
+AUTH_CHANNEL = int(env.get('AUTH_CHANNEL', ''))
+
+Dbclient = AsyncIOMotorClient(DB_URL)
+Cluster = Dbclient['Cluster0']
+Data = Cluster['users']
+Bot = Client(name='AutoApproveBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+async def get_fsub(bot, message):
+    target_channel_id = AUTH_CHANNEL  # Your channel ID
+    user_id = message.from_user.id
+    try:
+        # Check if user is a member of the required channel
+        await bot.get_chat_member(target_channel_id, user_id)
+    except UserNotParticipant:
+        # Generate the channel invite link
+        channel_link = (await bot.get_chat(target_channel_id)).invite_link
+        join_button = InlineKeyboardButton("🔔 Join Our Channel", url=channel_link)
+
+        # Display a message encouraging the user to join
+        keyboard = [[join_button]]
+        await message.reply(
+            f"<b>👋 Hello {message.from_user.mention()}, Welcome!</b>\n\n"
+            "📢 <b>Exclusive Access Alert!</b> ✨\n\n"
+            "To unlock all the amazing features I offer, please join our updates channel. "
+            "This helps us keep you informed and ensures top-notch service just for you! 😊\n\n"
+            "<i>🚀 Join now and dive into a world of knowledge and creativity!</i>",
+            reply_markup=InlineKeyboardMarkup(keyboard),
         )
+        return False
+    else:
+        return True
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username
-        self.force_channel = Config.FORCE_SUB
-        if Config.FORCE_SUB:
-            try:
-                link = await self.export_chat_invite_link(Config.FORCE_SUB)
-                self.invitelink = link
-            except Exception as e:
-                logging.warning(e)
-                logging.warning("Make Sure Bot admin in force sub channel")
-                self.force_channel = None
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
-        logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
+@Bot.on_message(filters.command("start") & filters.private)                    
+async def start_handler(c, m):
+    user_id = m.from_user.id
+    if not await Data.find_one({'id': user_id}):
+        await Data.insert_one({'id': user_id})
+    # Force Subscription Check
+    is_subscribed = await get_fsub(c, m)
+    if not is_subscribed:
+        return
+    button = [[
+        InlineKeyboardButton("⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆", url=f"https://telegram.me/Shanu_Auto_Approval_Bot?startgroup=true&admin=invite_users"),
+    ], [
+        InlineKeyboardButton("• ᴜᴩᴅᴀᴛᴇꜱ •", url="https://t.me/AeroBots_Tm"),
+        InlineKeyboardButton("• ꜱᴜᴩᴩᴏʀᴛ •", url="https://t.me/AeroBots_Group")
+    ], [
+        InlineKeyboardButton("⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⇆", url=f"https://telegram.me/Shanu_Auto_Approval_Bot?startchannel=true&admin=invite_users")
+    ]]
+    return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
 
-        for id in Config.ADMIN:
-            try:
-                await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
-            except:
-                pass
+@Bot.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))
+async def broadcast(c, m):
+    if m.text == "/users":
+        total_users = await Data.count_documents({})
+        return await m.reply(f"Total Users: {total_users}")
+    
+    b_msg = m.reply_to_message
+    if not b_msg:
+        return await m.reply("Please reply to a message to broadcast.")
+    
+    sts = await m.reply_text("Broadcasting your message...")
+    users = Data.find({})
+    total_users = await Data.count_documents({})
+    done, failed, success = 0, 0, 0
+    start_time = time.time()
 
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+    async for user in users:
+        user_id = int(user['id'])
+        try:
+            await b_msg.copy(chat_id=user_id)
+            success += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await b_msg.copy(chat_id=user_id)
+            success += 1
+        except (InputUserDeactivated, PeerIdInvalid):
+            await Data.delete_many({'id': user_id})
+            failed += 1
+        except UserIsBlocked:
+            failed += 1
+        except Exception as e:
+            # Optionally log unexpected errors for debugging
+            print(f"Error broadcasting to {user_id}: {e}")
+            failed += 1
+        done += 1
 
-    async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot Stopped 🙄")
+        # Update status for every user
+        await sts.edit(f"Broadcast in progress:\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+    
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
+    await sts.delete()
+    await m.reply_text(
+        f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\n"
+        f"Total Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}",
+        quote=True
+    )
 
-bot_instance = Bot()
+@Bot.on_chat_join_request()
+async def req_accept(c, m):
+    user_id = m.from_user.id
+    chat_id = m.chat.id
+    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
+    await c.approve_chat_join_request(chat_id, user_id)
+    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
+    except Exception as e: print(e)
 
-def main():
-    async def start_services():
-        if Config.STRING_SESSION:
-            await asyncio.gather(
-                app.start(),        # Start the Pyrogram Client
-                bot_instance.start()  # Start the bot instance
-            )
-        else:
-            await asyncio.gather(
-                bot_instance.start()
-            )
-        
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_services())
-    loop.run_forever()
-
-if __name__ == "__main__":
-    warnings.filterwarnings("ignore", message="There is no current event loop")
-    main()
-
+Bot.run()
--- config.py
+++ config.py
@@ -1,100 +1,90 @@
-import re
+#(©)t.me/CodeFlix_Bots
+
+
+
+
 import os
-import time
-
-id_pattern = re.compile(r'^.\d+$')
-
-
-class Config(object):
-    # pyro client config
-    API_ID = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
-
-    # premium 4g renaming client
-    STRING_API_ID = os.environ.get("STRING_API_ID", "")
-    STRING_API_HASH = os.environ.get("STRING_API_HASH", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
-
-    # database config
-    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    DB_URL = os.environ.get("DB_URL", "")  # ⚠️ Required
-
-    # other configs
-    BOT_UPTIME = time.time()
-    START_PIC = os.environ.get("START_PIC", "")
-    ADMIN = [int(admin) if id_pattern.search(
-        admin) else admin for admin in os.environ.get('ADMIN', '').split()]  # ⚠️ Required
-    
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") # ⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
-    FLOOD = int(os.environ.get("FLOOD", '10'))
-    BANNED_USERS = set(int(x) for x in os.environ.get(
-        "BANNED_USERS", "1234567890").split())
-
-    # wes response configuration
-    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8080"))
+import logging
+from logging.handlers import RotatingFileHandler
 
 
-class Txt(object):
-    # part of text configuration
-    START_TXT = """<b>Hᴀɪ {} 👋,
-Tʜɪs Is Aɴ Aᴅᴠᴀɴᴄᴇᴅ Aɴᴅ Yᴇᴛ Pᴏᴡᴇʀꜰᴜʟ Rᴇɴᴀᴍᴇ Bᴏᴛ
-Usɪɴɢ Tʜɪs Bᴏᴛ Yᴏᴜ Cᴀɴ Rᴇɴᴀᴍᴇ & Cʜᴀɴɢᴇ Tʜᴜᴍʙɴᴀɪʟ Oꜰ Yᴏᴜʀ Fɪʟᴇ
-Yᴏᴜ Cᴀɴ Aʟsᴏ Cᴏɴᴠᴇʀᴛ Vɪᴅᴇᴏ Tᴏ Fɪʟᴇ & Fɪʟᴇ Tᴏ Vɪᴅᴇᴏ
-Tʜɪs Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛs Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ
-"""
 
-    ABOUT_TXT = """<b>╭───────────⍟
-• ᴍy ɴᴀᴍᴇ : {}
-• ᴘʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Mikoyo>Mikoyo</a>
-• ɴᴇᴛᴡᴏʀᴋ : <a href=https://t.me/QuanticXNetwork>ᴏᴛᴀᴋᴜ</a> 
-• ᴍᴏᴠɪᴇs : <a href=https://t.me/Hindi_Anime_Movies_dubbed>ᴍᴏᴠɪᴇ</a>
-• sᴇʀɪᴇs : <a href=https://t.me/ShanuAnime>sᴇʀɪᴇsғʟɪx</a>
-• ᴀɴɪᴍᴇ: <a href=https://t.me/ShanuAnime>Shanu Anime</a>
-• ᴄʜᴀᴛ ɢʀᴏᴜᴘ: <a href=https://t.me/AeroBots_Group>Aero Chat</a>
-• ᴍʏ ꜱᴇʀᴠᴇʀ : <a href=https://t.me/AeroBots_Tm>ᴠᴘs</a>
-╰───────────────⍟ """
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    HELP_TXT = """
-🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
-  
-<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
-https://t.me/Hindi_Anime_Movies_dubbed
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
 
-📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
 
-<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
-<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
-<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
-Exᴀᴍᴩʟᴇ:- <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
-💾 Sɪᴢᴇ: {filesize}
-⏰ Dᴜʀᴀᴛɪᴏɴ: {duration} </code>
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001995978690"))
 
-✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
-<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+# NAMA OWNER
+OWNER = os.environ.get("OWNER", "VeldXd")
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
+
+#Port
+PORT = os.environ.get("PORT", "8030")
+
+#Database
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001473043276"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001644866777"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
+try:
+    ADMINS=[6376328008]
+    for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @OtakuFlix_Network</b>")
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(5191566338)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-<b>➜ ᴘᴏᴡᴇʀᴇᴅ ʙʏ:</b> <a href=https://t.me/AeroBots_Tm>Aero Bots</a>
-"""
-
-    SEND_METADATA = """
-❪ SET CUSTOM METADATA ❫
-
-☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
-
-◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @team_netflix" -metadata author="@Codeflix_bots" -metadata:s:s title="Subtitled By :- @Team_netflix" -metadata:s:a title="By :- @team_netflix" -metadata:s:v title="By:- @Codeflix_bots" </code>
-
-📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @AeroBots_Tm
-"""
-
-    PROGRESS_BAR = """<b>\n
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱➜
-➜ 🗃️ sɪᴢᴇ: {1} | {2}
-➜ ⏳️ ᴅᴏɴᴇ : {0}%
-➜ 🚀 sᴘᴇᴇᴅ: {3}/s
-➜ ⏰️ ᴇᴛᴀ: {4}
-╰━━━━━━━━━━━━━━━➜ </b>"""
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+   
